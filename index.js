58eb591c65901b28aa8d15f33f0b6f33f8093531
Brenden West <brendenw@hotmail.com>
2021-10-26 20:32:05 -0700

    parse daily ride & location

--- index.js
+++ index.js
@@ -8,15 +8,48 @@ app.use(express.static(__dirname + '/public')); // allows direct navigation to s
 
 const BASE_URL = "https://cascade.org/";
 
-const getRide = (element) => {
+const getRides = (element) => {
     let title = $(element).find("h3").text()
     let url = $(element).find("a").attr("href")
     let date = $(element).find(".date-display-single").text()
     let pace = $(element).find(".field-name-field-daily-pace").text()
     let distance = $(element).find(".field-name-field-daily-distance").text()
     let leader = $(element).find(".field-name-field-daily-rideleader").text()
-    let locations = $(element).find(".field-name-field-location-address").text()
-    return { title, url, date, pace, distance, leader, locations}
+    let location = getLocation($(element).find(".field-name-field-location-address"));
+    return { title, url, date, pace, distance, leader, location}
+}
+
+const getLocation = (element) => {
+    return {
+     "street": `${$(element).find(".thoroughfare").text()}, ${$(element).find(".premise").text()}`,
+     "city": $(element).find(".locality").text(),
+     "state": $(element).find(".state").text(),
+     "postal-code": $(element).find(".postal-code").text(),
+     "google-map-link": $(element).find(".google-map-link").find("a").attr("href")
+    }
+}
+
+const getDailyRide = (element) => {
+    let title = $(element).find("h3").text()
+
+    let pace = $(element).find(".field-name-field-daily-pace").find(".field-item").text();
+    let distance = $(element).find(".field-name-field-daily-distance").find(".field-item").text();
+    let elevation = $(element).find(".field-name-field-daily-elevation").find(".field-item").text();
+    let datetime = $(element).find(".field-name-field-global-datetime").find(".field-item").text();
+    let terrain = $(element).find(".field-name-field-daily-terrain").find(".field-item").text();
+    let regroup = $(element).find(".field-name-field-daily-regroup").find(".field-item").text();
+    let weather = $(element).find(".field-name-field-daily-weather").find(".field-item").text();
+    let interests = $(element).find(".field-name-field-global-interests").find(".field-item").text();
+    let description = $(element).find(".field-name-body").find(".field-item").text();
+    let links = $(element).find(".field-name-field-daily-links").find("a").attr("href");
+    let contact = {
+        "name": $(element).find(".field-name-field-contact-name").text(),
+        "email": $(element).find(".field-name-field-contact-email").text(),
+        "phone": $(element).find(".field-name-field-contact-phone").text()
+    }
+    let location = getLocation($(element).find(".field-name-field-location-address"))
+
+    return { title, datetime, pace, distance, elevation, terrain, regroup, weather, interests, description, links, contact, location}
 }
 
 const getNews = (element) => {
@@ -35,14 +68,16 @@ app.get('/', (req,res) => {
     res.send('home');
 });
 
-app.get('/:section', (req,res, next) => {
-    const url = `${BASE_URL}${req.params.section}/`
+app.get('/:section/:id?', (req,res, next) => {
+    let url = `${BASE_URL}${req.params.section}/`
+    if (req.params.id) {
+        url = url + req.params.id;
+    }
     console.log(url)
 
     https.get(url, (response) => {
 
         let data = [];
-        // { title: "", image_url: "", description: "", url: ""}
 
         // Continuously update stream with data
         let body = '';
@@ -55,14 +90,20 @@ app.get('/:section', (req,res, next) => {
             switch (req.params.section) {
               case 'calendar':
                 $('.views-row').each((index, element) => {
-                    item = getRide(element);
+                    item = getRides(element);
                     data.push(item)
                 });
+                break;
               case 'blog':
                 $('.node-blog').each((index, element) => {
                     item = getNews(element);
                     data.push(item)
                 });
+                break;
+              case 'node':
+                data = getDailyRide($('.node-daily-ride'));
+                data['title'] = $('.page-title').text();
+                break;
               default:
                 console.log('no match');
                 }
